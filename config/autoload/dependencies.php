4de01bd628f65b3c8e734a8dcde2bdb3de4f556c
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-04-12 16:57:59 +0800

    refactor: 移除 dependencies.php 中的 SSE 相关类配置

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -9,9 +9,5 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-use FriendsOfHyperf\MCP\Contract\SseServerTransport;
-use FriendsOfHyperf\MCP\Transport\RedisSseServerTransport;
-
 return [
-    // SseServerTransport::class => RedisSseServerTransport::class,
 ];
